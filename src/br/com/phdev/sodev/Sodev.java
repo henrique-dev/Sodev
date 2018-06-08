2d3501bd6c8cc5e938ebccc2c5ec1504785c23b5
Paulo Henrique <henrique.phgb@gmail.com>
2018-06-07 21:26:17 -0300

    Merge origin/master
    Conflicts:
    	src/main/webapp/WEB-INF/views/responsavel/vizualiza-notas-aula.jsp

--- src/br/com/phdev/sodev/Sodev.java
+++ src/br/com/phdev/sodev/Sodev.java
@@ -5,17 +5,33 @@
  */
 package br.com.phdev.sodev;
 
+import br.com.phdev.sodev.driver.HCSR04;
+import br.com.phdev.sodev.driver.MPU9150;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.RaspiPin;
+
 /**
  *
  * @author Paulo Henrique Gonçalves Bacelar
  */
 public class Sodev {
 
-    /**
-     * @param args the command line arguments
-     */
+    private HCSR04 MOD_01;
+    private HCSR04 MOD_02;
+    private MPU9150 ACC_01;
+    
+    public Sodev() {
+        this.MOD_01 = new HCSR04(RaspiPin.GPIO_06, RaspiPin.GPIO_10, RaspiPin.GPIO_11);
+        //this.MOD_02 = new HCSR04(RaspiPin.GPIO_15, RaspiPin.GPIO_16, RaspiPin.GPIO_01);        
+    }
+    
+    public void start() {
+        this.MOD_01.start();
+        //this.MOD_02.start();
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Sodev().start();
     }
     
 }
