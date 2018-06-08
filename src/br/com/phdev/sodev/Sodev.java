97d0f172f67501a6f996d429f5b848f4e59211f1
Paulo Henrique <henrique.phgb@gmail.com>
2018-06-08 09:15:15 -0300

    Correções

--- src/br/com/phdev/sodev/Sodev.java
+++ src/br/com/phdev/sodev/Sodev.java
@@ -20,6 +20,7 @@ public class Sodev {
     private static final int SENSOR_01 = 0;
     private static final int SENSOR_02 = 1;
     private static final int ALL_SENSORS = 2;
+    private static final int NO_SENSORS = 3;
 
     private HCSR04 MOD_01;
     private HCSR04 MOD_02;
@@ -40,6 +41,7 @@ public class Sodev {
                     this.MOD_01 = new HCSR04(RaspiPin.GPIO_06, RaspiPin.GPIO_10, RaspiPin.GPIO_11);
                     this.MOD_02 = new HCSR04(RaspiPin.GPIO_15, RaspiPin.GPIO_16, RaspiPin.GPIO_01);
                     break;
+                case NO_SENSORS:
             }
             if (enableAccel) {
                 this.ACC_01 = new MPU9150();
@@ -50,8 +52,12 @@ public class Sodev {
     }
 
     public void start() {
-        this.MOD_01.start();
-        this.MOD_02.start();
+        if (this.MOD_01 != null) {
+            this.MOD_01.start();
+        }
+        if (this.MOD_01 != null) {
+            this.MOD_02.start();
+        }
     }
 
     public HCSR04 getMOD_01() {
@@ -71,51 +77,53 @@ public class Sodev {
         int sensor = -1;
         boolean acc = false;
         boolean moduleInfo = false;
-        switch (args[0] + args[1]) {
-            case "-s1":
+        switch (args[0]) {
+            case "1":
                 sensor = SENSOR_01;
                 break;
-            case "-s2":
+            case "2":
                 sensor = SENSOR_02;
                 break;
-            case "-sboth":
+            case "both":
                 sensor = ALL_SENSORS;
                 break;
+            case "off":
+                sensor = NO_SENSORS;
+                break;
+            default:
+                System.exit(-1);
         }
 
-        if ((args[2] + args[3]).equals("-itrue")) {
-            moduleInfo = true;
-        }
-        if ((args[4] + args[5]).equals("-aon")) {
-            acc = true;
+        switch (args[1]) {
+            case "true":
+                moduleInfo = true;
+                break;
+            case "false":
+                moduleInfo = false;
+                break;
+            default:
+                System.exit(-1);
         }
 
-        if (sensor == -1) {
-            System.exit(0);
+        switch (args[2]) {
+            case "on":
+                acc = true;
+                break;
+            case "off":
+                acc = false;
+                break;
+            default:
+                System.exit(-1);
         }
 
         Sodev sodev = new Sodev(sensor, moduleInfo, acc);
         sodev.start();
 
         if (acc) {
-            //double maxAngle = 0;
-            //double minAngle = 0;
-
             while (true) {
                 List<Double> values = sodev.ACC_01.readAccel();
-                //double y = (values.get(1) + 90) * -1;
-                double y = values.get(1);
-                /*
-            if (y > maxAngle) {
-                maxAngle = y;
-                System.out.println("ANGULO MAXIMO: " + maxAngle);
-            }
-            if (y < minAngle) {
-                minAngle = y;
-                System.out.println("ANGULO MINIMO: " + minAngle);
-            }*/
+                double y = (values.get(1) + 90) * -1;
                 System.out.println("Y: " + y);
-                //System.out.println("X: " + values.get(0) + "    Y: " + ((values.get(1) + 90)) + "    Z: " + values.get(2));
             }
         }
     }
