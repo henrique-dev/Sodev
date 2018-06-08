a6975f0e004efaf12aa7e42d3cce2b6ccf3bd8cb
Paulo Henrique <henrique.phgb@gmail.com>
2018-06-07 22:06:42 -0300

    Merge origin/master
    Conflicts:
    	src/main/webapp/WEB-INF/views/responsavel/vizualiza-notas-aula.jsp

--- src/br/com/phdev/sodev/Sodev.java
+++ src/br/com/phdev/sodev/Sodev.java
@@ -59,7 +59,14 @@ public class Sodev {
         while (true) {
             List<Double> values = sodev.ACC_01.readAccel();
             double y = (values.get(1) + 90) * -1;
-            
+            if (y > maxAngle) {
+                maxAngle = y;
+                System.out.println("ANGULO MAXIMO: " + maxAngle);
+            }
+            if (y < minAngle) {
+                minAngle = y;
+                System.out.println("ANGULO MINIMO: " + minAngle);
+            }
             //System.out.println("X: " + values.get(0) + "    Y: " + ((values.get(1) + 90)) + "    Z: " + values.get(2));
         }
     }        
