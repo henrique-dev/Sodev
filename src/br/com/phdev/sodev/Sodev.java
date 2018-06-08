b1ff98beba3e7daa6361f12cd9bd89d5918ff4e7
Paulo Henrique <henrique.phgb@gmail.com>
2018-06-07 21:52:43 -0300

    Merge origin/master
    Conflicts:
    	src/main/webapp/WEB-INF/views/responsavel/vizualiza-notas-aula.jsp

--- src/br/com/phdev/sodev/Sodev.java
+++ src/br/com/phdev/sodev/Sodev.java
@@ -9,30 +9,53 @@ import br.com.phdev.sodev.driver.HCSR04;
 import br.com.phdev.sodev.driver.MPU9150;
 import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.RaspiPin;
+import java.util.List;
 
 /**
  *
  * @author Paulo Henrique Gonçalves Bacelar
  */
-public class Sodev {
+public class Sodev {        
 
     private HCSR04 MOD_01;
     private HCSR04 MOD_02;
     private MPU9150 ACC_01;
-    
+
     public Sodev() {
-        this.MOD_01 = new HCSR04(RaspiPin.GPIO_06, RaspiPin.GPIO_10, RaspiPin.GPIO_11);
-        this.MOD_01.setShowDistance(true);
-        this.MOD_02 = new HCSR04(RaspiPin.GPIO_15, RaspiPin.GPIO_16, RaspiPin.GPIO_01);        
+        try {
+            this.MOD_01 = new HCSR04(RaspiPin.GPIO_06, RaspiPin.GPIO_10, RaspiPin.GPIO_11);
+            this.MOD_01.setShowDistance(true);
+            this.MOD_02 = new HCSR04(RaspiPin.GPIO_15, RaspiPin.GPIO_16, RaspiPin.GPIO_01);
+            this.ACC_01 = new MPU9150();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
+
     public void start() {
-        this.MOD_01.start();
-        this.MOD_02.start();
+        //this.MOD_01.start();
+        //this.MOD_02.start();
     }
-    
+
+    public HCSR04 getMOD_01() {
+        return MOD_01;
+    }
+
+    public HCSR04 getMOD_02() {
+        return MOD_02;
+    }
+
+    public MPU9150 getACC_01() {
+        return ACC_01;
+    }        
+
     public static void main(String[] args) {
-        new Sodev().start();
-    }
-    
+        Sodev sodev = new Sodev();
+        //sodev.start();
+        while (true) {
+            List<Double> values = sodev.ACC_01.readAccel();
+            System.out.println("X: " + values.get(0) + "    Y: " + values.get(1) + "    Z: " + values.get(2));
+        }
+    }        
+
 }
