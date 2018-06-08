263a2bf01b73077253eb1a4ec6005c98c2369a3c
Paulo Henrique <henrique.phgb@gmail.com>
2018-06-08 11:06:04 -0300

    Correções

--- src/compilar.sh
+++ src/compilar.sh
@@ -2,4 +2,4 @@
 
 echo "COMPILANDO"
 
-javac -cp .:classes:/opt/pi4j/lib/'*' br/com/phdev/sodev/*.java br/com/phdev/sodev/driver/*.java
+javac -cp .:classes:/opt/pi4j/lib/'*' br/com/phdev/sodev/*.java br/com/phdev/sodev/driver/*.java br/com/phdev/sodev/connection/*.java 
